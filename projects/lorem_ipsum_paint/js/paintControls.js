32e5aa53b11c6103c5c5496fbd7e885efc8937b7
Dominic Ginger <domginger@hotmail.co.uk>
2016-11-15 21:24:37 +0000

    Make ipsum paint work on touch devices

--- projects/lorem_ipsum_paint/js/paintControls.js
+++ projects/lorem_ipsum_paint/js/paintControls.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
   var canvas = document.getElementById(id);
   var context = canvas.getContext('2d');
   var painting = false;
-  var words= ["Lorem", "ipsum", "dolor", "sit", "ame", ", consectetur", "adipiscing", "eli", ", sed", "do", "eiusmod", "tempor", "incididunt", "ut", "labore", "et",
-  "dolore", "magna", "aliqu", ". Ut", "enim", "ad", "minim", "venia", ", quis", "nostrud", "exercitation", "ullamco", "laboris", "nisi", "ut", "aliquip", "ex", "ea",
-  "commodo", "consequa", ". Duis", "aute", "rure", "olor", "n", "eprehenderit", "n", "oluptate", "elit", "sse", "illum", "olore", "eu", "fugiat", "nulla", "pariatu",
-  ". Excepteur","sint", "occaecat", "cupidatat", "non", "proiden", ", sunt", "in", "culpa", "qui", "officia", "deserunt", "mollit", "anim", "id", "est", "laborum."];
+  var words= ['Lorem', 'ipsum', 'dolor', 'sit', 'ame', ', consectetur', 'adipiscing', 'eli', ', sed', 'do', 'eiusmod', 'tempor', 'incididunt', 'ut', 'labore', 'et',
+  'dolore', 'magna', 'aliqu', '. Ut', 'enim', 'ad', 'minim', 'venia', ', quis', 'nostrud', 'exercitation', 'ullamco', 'laboris', 'nisi', 'ut', 'aliquip', 'ex', 'ea',
+  'commodo', 'consequa', '. Duis', 'aute', 'rure', 'olor', 'n', 'eprehenderit', 'n', 'oluptate', 'elit', 'sse', 'illum', 'olore', 'eu', 'fugiat', 'nulla', 'pariatu',
+  '. Excepteur','sint', 'occaecat', 'cupidatat', 'non', 'proiden', ', sunt', 'in', 'culpa', 'qui', 'officia', 'deserunt', 'mollit', 'anim', 'id', 'est', 'laborum.'];
   function getWord(words) {
     return words[Math.floor(Math.random() * words.length)];
   }
@@ -14,33 +14,34 @@ $(document).ready(function() {
     var params = { x: x, y: y, ctx: context, word: getWord(words) }
     PaintCore.paint(params);
   }
+  function setSize() {
+    context.canvas.width = window.innerWidth;
+    context.canvas.height = window.innerHeight;
+  }
 
-  context.canvas.width = window.innerWidth;
-  context.canvas.height = window.innerHeight;
+  setSize();
   context.lineWidth = 2;
-  context.font="20px monospace";
+  context.font='20px monospace';
 
-  $('#' + id).mousedown(function(e) {
+  $(window).resize(function(e) {
+    setSize();
+  });
+
+  $('#' + id).on('mousedown touchstart', function(e) {
     painting = true;
     paint(e.pageX, e.pageY);
   });
-  $('#' + id).on('touchstart', function(e) {
-    painting = true;
-    paint(e.pageX, e.pageY);
+
+  $('#' + id).on('mousemove', function(e) {
+    if (painting) paint(e.pageX, e.pageY);
   });
 
-  $('#' + id).mousemove(function(e) {
-    if (painting) {
-      paint(e.pageX, e.pageY);
-    }
-  });
   $('#' + id).on('touchmove', function(e) {
-    if (painting) {
-      paint(e.pageX, e.pageY);
-    }
+    var t = e.changedTouches[0]
+    if (painting) paint(t.clientX, t.clientY);
   });
 
-  $('#' + id).on('mouseup mouseleave touchend touchcancel', function(e) {
+  $('#' + id).on('mouseup mouseleave touchend', function(e) {
     painting = false;
   });
 });
