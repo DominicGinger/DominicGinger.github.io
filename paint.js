dba3f99197373eeab201f0b927c549f2a8f063e8
Dominic Ginger <domginger@hotmail.co.uk>
2017-08-01 23:23:50 +0100

    Tidy up and try a few things out

--- paint.js
+++ paint.js
@@ -1,16 +1,15 @@
-var showSettings = true;
-var canvas;
-var context;
-var textWidth = 12;
-var textHeight = 20;
-var drawingFont = '20px monospace';
-var usedPositions = [];
+let canvas;
+let context;
+const textWidth = 12;
+const textHeight = 20;
+const drawingFont = '20px monospace';
+let usedPositions = [];
 
 document.addEventListener("DOMContentLoaded", function(event) { 
   canvas = document.querySelector('#paintMe');
   context = canvas.getContext('2d');
-  var painting = false;
-  var words= ['Lorem', 'ipsum', 'dolor', 'sit', 'ame', ', consectetur', 'adipiscing', 'eli', ', sed', 'do', 'eiusmod', 'tempor', 'incididunt', 'ut', 'labore', 'et',
+  let painting = false;
+  const words= ['Lorem', 'ipsum', 'dolor', 'sit', 'ame', ', consectetur', 'adipiscing', 'eli', ', sed', 'do', 'eiusmod', 'tempor', 'incididunt', 'ut', 'labore', 'et',
     'dolore', 'magna', 'aliqu', '. Ut', 'enim', 'ad', 'minim', 'venia', ', quis', 'nostrud', 'exercitation', 'ullamco', 'laboris', 'nisi', 'ut', 'aliquip', 'ex', 'ea',
     'commodo', 'consequa', '. Duis', 'aute', 'rure', 'olor', 'n', 'eprehenderit', 'n', 'oluptate', 'elit', 'sse', 'illum', 'olore', 'eu', 'fugiat', 'nulla', 'pariatu',
     '. Excepteur','sint', 'occaecat', 'cupidatat', 'non', 'proiden', ', sunt', 'in', 'culpa', 'qui', 'officia', 'deserunt', 'mollit', 'anim', 'id', 'est', 'laborum.'];
@@ -24,20 +23,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
   }
 
   function canDraw(newX, newY, length) {
-    var x2 = newX + (textWidth*length);
+    const x2 = newX + (textWidth*length);
 
     return !usedPositions.some(function(pos) {
-      var r = pos.x;
-      var r2 = pos.x + (textWidth*pos.length);
+      const r = pos.x;
+      const r2 = pos.x + (textWidth*pos.length);
 
       return (newY === pos.y && ((newX <= r2) && (r <= x2)));
     });
   }
 
   function paint(x, y, word) {
-    var offset = textWidth + (word.length * textWidth);
-    var newX = (Math.round((x - textWidth) / offset) * offset);
-    var newY = (Math.round((y - textHeight) / textHeight) * textHeight) + textHeight;
+    const offset = textWidth + (word.length * textWidth);
+    const newX = (Math.round((x - textWidth) / offset) * offset);
+    const newY = (Math.round((y - textHeight) / textHeight) * textHeight) + textHeight;
 
     if(canDraw(newX, newY, word.length)) {
       draw(word, newX, newY);
@@ -64,7 +63,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   canvas.addEventListener('touchmove', function(e) {
     e.preventDefault();
-    var t = e.changedTouches[0]
+    const t = e.changedTouches[0]
     if (painting) paint(t.clientX, t.clientY, getWord());
   });
 
@@ -75,10 +74,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
   canvas.addEventListener('mouseup', mouseUp);
   document.addEventListener('mouseleave', mouseUp);
   canvas.addEventListener('touchend', mouseUp);
-});
 
-function draw(word, paintX, paintY) {
-  context.fillText(word, paintX, paintY);
-  usedPositions.push({ x: paintX, y: paintY, length: word.length, word });
-}
+  function draw(word, paintX, paintY) {
+    context.fillText(word, paintX, paintY);
+    usedPositions.push({ x: paintX, y: paintY, length: word.length, word, time: new Date() });
+  }
+
+  (function loop() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    const timeNow = new Date();
+    usedPositions = usedPositions.filter(pos => new Date() - pos.time < 1000);
+    usedPositions.forEach(pos => {
+      context.globalAlpha = 1 - ((timeNow - pos.time)/1000);
+      context.fillText(pos.word, pos.x, pos.y);
+    });
+
+    requestAnimationFrame(loop); 
+  })();
+
+  (function randomAdd() {
+    const card = document.querySelector('.inner').getBoundingClientRect();
+      const randomX = Math.floor(Math.random() * window.innerWidth);
+      const randomY = Math.floor(Math.random() * window.innerHeight);
+      draw(getWord(), randomX, randomY);
+    setTimeout(() => requestAnimationFrame(randomAdd), 600);
+  });
+});
 
